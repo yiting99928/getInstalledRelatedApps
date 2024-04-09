7ada771132bf95a30d7e532c34a094cf1d7c2500
Yumy.Yang <yumy.yang@ehsn.com.tw>
2024-04-09 16:45:24 +0800

    add - getInstalledRelatedApps test

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-console.log("test");
 
-document.write("哈哈");
 async function checkInstalledRelatedApps() {
+  const outputDiv = document.getElementById("output");
+
   if (navigator.getInstalledRelatedApps) {
     try {
       const relatedApps = await navigator.getInstalledRelatedApps();
-        document.write(`relatedApps：${relatedApps}`)
+
       if (relatedApps && relatedApps.length > 0) {
         relatedApps.forEach((app) => {
-          document.write(`relatedApps：${app.id}, ${app.platform}, ${app.url}<br>`);
+          const appInfo = `${app.id}, ${app.platform}, ${app.url}<br>`;
+          outputDiv.innerHTML += appInfo;
         });
       } else {
-        document.write("未找到已安裝的相關應用程式");
+        outputDiv.innerHTML = "未找到已安裝的相關應用程式";
       }
     } catch (error) {
-      document.write("獲取已安裝應用程式時出錯：", error);
+      outputDiv.innerHTML = "獲取已安裝應用程式時出錯：" + error;
     }
   } else {
-    document.write("不支援 navigator.getInstalledRelatedApps() 方法");
+    outputDiv.innerHTML = "不支援 navigator.getInstalledRelatedApps() 方法";
   }
 }
 
