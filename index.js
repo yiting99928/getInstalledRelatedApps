c91d3b60148fffe7e83652b8884fe2f98d1a5017
Yumy.Yang <yumy.yang@ehsn.com.tw>
2024-04-09 15:47:19 +0800

    add getInstalledRelatedApps

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(666)
+console.log("test");
+
+async function checkInstalledRelatedApps() {
+  if (navigator.getInstalledRelatedApps) {
+    try {
+      const relatedApps = await navigator.getInstalledRelatedApps();
+
+      relatedApps.forEach((app) => {
+        console.log(app.id, app.platform, app.url);
+      });
+    } catch (error) {
+      console.error("獲取已安装應用程式時：", error);
+    }
+  } else {
+    console.error("不支持 navigator.getInstalledRelatedApps() 方法。");
+  }
+}
+
+checkInstalledRelatedApps();
