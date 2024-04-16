0d754a07f139ec341052cedca78c1ef55d4ae43c
Yumy.Yang <yumy.yang@ehsn.com.tw>
2024-04-16 17:01:53 +0800

    add - fix smartBannerButton

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-async function checkInstalledRelatedApps() {
-  const outputDiv = document.getElementById("output");
-
-  if (navigator.getInstalledRelatedApps) {
-    try {
-      const relatedApps = await navigator.getInstalledRelatedApps();
-
-      if (relatedApps && relatedApps.length > 0) {
-        relatedApps.forEach((app) => {
-          const appInfo = `${app.id}, ${app.platform}, ${app.url}<br>`;
-          outputDiv.innerHTML += appInfo;
-        });
-      } else {
-        outputDiv.innerHTML = "未找到已安裝的相關應用程式";
-      }
-    } catch (error) {
-      outputDiv.innerHTML = "獲取已安裝應用程式時出錯：" + error;
-    }
-  } else {
-    outputDiv.innerHTML = "不支援 navigator.getInstalledRelatedApps() 方法";
-  }
-}
-
-checkInstalledRelatedApps();
-
-const userAgent = document.getElementById("userAgent");
-userAgent.innerHTML = navigator.userAgent;
-
 var url =
   window.location.origin + window.location.pathname + window.location.search;
 var downloadAPP = false;
@@ -43,11 +15,12 @@ const isAppleDevice = function () {
   return /iphone|ipad/gi.test(navigator.appVersion)
 }
 
+
 window.onload = function () {
   var smartBannerButton = document.querySelector(".smartbanner__button");
   smartBannerButton.addEventListener("click", function () {
     // call URL Scheme
-    redirectTo(urlScheme);
+    // redirectTo(urlScheme);
     // appStore download
     var timer = setTimeout(function () {
       redirectProcess();
