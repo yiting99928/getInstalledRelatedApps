8328ba1cf879584eea37ae650b08a3c388fad8e6
Yumy.Yang <yumy.yang@ehsn.com.tw>
2024-04-16 16:29:37 +0800

    add - smartBannerButton onClick

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 async function checkInstalledRelatedApps() {
   const outputDiv = document.getElementById("output");
 
@@ -25,4 +24,77 @@ async function checkInstalledRelatedApps() {
 checkInstalledRelatedApps();
 
 const userAgent = document.getElementById("userAgent");
-userAgent.innerHTML = navigator.userAgent
+userAgent.innerHTML = navigator.userAgent;
+
+var url =
+  window.location.origin + window.location.pathname + window.location.search;
+var downloadAPP = false;
+// var urlScheme = "etmall://redirect?url=" + encodeURIComponent(url);
+var urlScheme = "etmall://open?pgid=2";
+var iosDownloadUrl =
+  "https://itunes.apple.com/tw/app/dong-sen-gou-wu-ehs-nin-sui/id974433138?l=zh&mt=8";
+var androidDownloadUrl =
+  "https://play.google.com/store/apps/details?id=com.hyxen.app.etmall";
+var isAppleDevice = utilityJS.isAppleDevice();
+var appStoreUrl = isAppleDevice ? iosDownloadUrl : androidDownloadUrl;
+var userAgent = navigator.userAgent;
+
+window.onload = function () {
+  var smartBannerButton = document.querySelector(".smartbanner__button");
+  smartBannerButton.addEventListener("click", function () {
+    // call URL Scheme
+    redirectTo(urlScheme);
+    // appStore download
+    var timer = setTimeout(function () {
+      redirectProcess();
+    }, 1000);
+    // 切換 APP 時，取消後續 Redirect 流程
+    document.addEventListener("visibilitychange", clearRedirectCallback, false);
+    window.addEventListener("pagehide", clearRedirectCallback, false);
+
+    function redirectProcess() {
+      // 有載 app || Safari 直接轉導 URL
+      if (downloadAPP) {
+        redirectTo(url);
+      } else if (window.confirm("是否下載東森APP或以手機版網頁開啟")) {
+        redirectToStore();
+      } else {
+        redirectTo(url);
+      }
+    }
+
+    function redirectToStore() {
+      redirectTo(appStoreUrl);
+
+      // @* 延長秒數到 1000ms，避免出現 android 手機沒有跳轉的情形 *@
+      var timer = setTimeout(function () {
+        redirectTo(url);
+      }, 1000);
+    }
+
+    function clearRedirectCallback() {
+        clearTimeout(timer)
+        document.querySelector('#result').innerText = '請關閉視窗';
+    };
+
+    function redirectTo(target) {
+      if (isAppleDevice) {
+        var a = document.createElement("a");
+        a.setAttribute("href", target);
+        a.click();
+      } else if (
+        /Line/.test(userAgent) ||
+        /Chrome/.test(userAgent) ||
+        /Firefox/.test(userAgent)
+      ) {
+        location.href = target;
+      } else {
+        var iframe = document.createElement("iframe");
+        iframe.frameBorder = "0";
+        iframe.style.cssText = "display:none;border:0;width:0;height:0;";
+        document.body.append(iframe);
+        iframe.src = target;
+      }
+    }
+  });
+};
